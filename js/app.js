eb7b1cdb87d7f8a14a2cbb831ea18ae18308420a
Raven Robertson <ravewillow6383@gmail.com>
2019-04-04 14:25:07 -0700

    small edits

--- js/app.js
+++ js/app.js
@@ -4,16 +4,17 @@ alert('Hi! Welcome to my page. I have a few questions for you to see how well yo
 
 var userName = prompt('First, What is your name?');
 var correctAnswers = 0;
+function questionOne() {
+  var anySiblings = prompt('Do I have any siblings? ').toLowerCase();
 
-var anySiblings = prompt('Do I have any siblings? ').toLowerCase();
-
-if(anySiblings === 'y' || anySiblings === 'yes') {
-  alert('You guessed it! I\'m the youngest of SEVEN kids!');
-  correctAnswers++;
-} else if(anySiblings === 'n' || anySiblings === 'no') {
-  alert('You got it all wrong!');
-} else {
-  alert('This was a yes or no question. How did you mess that up?');
+  if(anySiblings === 'y' || anySiblings === 'yes') {
+    alert('You guessed it! I\'m the youngest of SEVEN kids!');
+    correctAnswers++;
+  } else if(anySiblings === 'n' || anySiblings === 'no') {
+    alert('You got it all wrong!');
+  } else {
+    alert('This was a yes or no question. How did you mess that up?');
+  }
 }
 
 console.log('Does the user think that I have any siblings: ' , anySiblings);
@@ -71,10 +72,8 @@ if(playLotto === 'y' || playLotto === 'yes') {
 console.log('Does the user think that I play the lotto: ' , playLotto);
 
 var numberOfTattoos = Number(prompt('I will give you five chances at guessing how many tattoos I have!'));
-
-console.log('Checking prompt on first use input for how many tats they think I have:' , numberOfTattoos);
-
 var guessesRemaining = 5;
+console.log('Checking prompt on first use input for how many tats they think I have:' , numberOfTattoos);
 
 // I am writing my 'if' statement and while loop this way because it's the only format that I tried that finally worked. Although I feel as though the first if and the 2nd if should be able to be combined, I couldn't get my code to loop proberly and break the loop properly when I only had the first or third 'if' statements as opposed to both.
 if(numberOfTattoos === 6) {
@@ -89,6 +88,7 @@ if(numberOfTattoos === 6) {
     if(numberOfTattoos < 6) {
       alert('Close, but you are guessing too low.');
     }
+    // Here on line 93, when I didn't specify that the prompt should be a number, the script was reading the variable as a string even when it was just a numeric input. Perhaps could of used parseInt?
     numberOfTattoos = Number(prompt('Guess again! You have ' + guessesRemaining + ' more tries.'));
     if(guessesRemaining > 0 && numberOfTattoos === 6) {
       alert('Correct! I have spent more money than I care to discuss on my six tattoos!');
@@ -96,14 +96,14 @@ if(numberOfTattoos === 6) {
   }
 }
 
-alert('You already know that I have a dog. I love animals and I have had a variety of pets in my life!');
+alert('We\'ve already talked about my dog but I have actually had a variety of pets in my life.');
 
 var petsIHad = ['ferret', 'crow', 'pigeon', 'bird', 'lizard', 'gerbil'];
 var numberOfGuesses = 5;
 
 // I was having trouble being able to break out of this while loop. I tried inputting the word, break, on line107. It did not work. I tried adding '&& guessWhichpet !== petsIhad.length' onto line 98, but since I had not yet run the code  to check through my array, the condition to run the loop was never true. So I ended up changing the number of guesses to 0 on line 107, which closes the loop with a correct answer.
 while(numberOfGuesses > 0) {
-  var guessWhichPet = prompt('Name an animal, besides a dog, that you think I may have had for a pet in my life. One word only and please make don\'t make it plural! My code won\'t accept your answer if there\'s an \'s\' at the end! I will give you 5 guesses!').toLowerCase();
+  var guessWhichPet = prompt('Name an animal, besides a dog, that you think I may have had for a pet. One word only and please make don\'t make it plural! My code won\'t accept your answer if there\'s an \'s\' at the end! I will give you 5 guesses!').toLowerCase();
   console.log('initial type of pet prompt response' , guessWhichPet);
   numberOfGuesses--;
   for(var i = 0; i < petsIHad.length; i++) {
@@ -124,12 +124,12 @@ while(numberOfGuesses > 0) {
   }
 }
 
-// To tell the users all of the possible correct answers
+// To tell the users all of the possible correct answers.
 for(var j=0; j < petsIHad.length; j++) {
   alert(petsIHad[j]);
 }
 
-console.log('checking correct answers math:' , correctAnswers)
+console.log('checking correct answers math:' , correctAnswers);
 
 if(correctAnswers < 4){
   alert('Hey, ' + userName + ', you only got ' + correctAnswers + ' out of 7 right! You need to start paying more attention to me.');
